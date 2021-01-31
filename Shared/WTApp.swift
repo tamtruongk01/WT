feb0091a383dbc71f5e3d87f02ca278ceae9fcf1
Tam Truong <tamtruongk01@gmail.com>
2021-01-30 23:34:33 -0500

    Initial Commit

---
+++ Shared/WTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTApp.swift
+//  Shared
+//
+//  Created by Tam Truong on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct WTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
